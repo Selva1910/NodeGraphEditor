4df596a63753c83e4e4abb8bf70313933261dd99
bselva <bselva1910@gmail.com>
2026-02-22 22:54:08 +0530

    updated version number to be from package json

--- Editor/NodeGraphEditorAbout.cs
+++ Editor/NodeGraphEditorAbout.cs
@@ -7,11 +7,19 @@ namespace NodeGraph.Editor
 public class NodeGraphEditorAbout : EditorWindow {
    
     public static void ShowWindow() {
-        EditorUtility.DisplayDialog("About Node Graph Editor", 
-            "Developed by Selvaraj Balakrishnan\n" +
-            "Version: 1.0.5\n\n" +
-            "This software is licensed under AGPLv3. " +
-            "Modifications must remain open source and credit the original author.", "OK");
+        var packageInfo = UnityEditor.PackageManager.PackageInfo.FindForAssembly(typeof(NodeGraphEditorAbout).Assembly);
+
+        if (packageInfo != null) 
+        {
+            EditorUtility.DisplayDialog($"About {packageInfo.displayName}", 
+                $"Developed by {packageInfo.author.name}\n" +
+                $"Version: {packageInfo.version}\n\n" +
+                "This software is licensed under AGPLv3.", "OK");
+        } 
+        else 
+        {
+            EditorUtility.DisplayDialog("About Node Graph Editor", "Metadata not found. Ensure the script is inside a UPM package folder with an .asmdef file.", "OK");
+        }
     }
 }
 }
